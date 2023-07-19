a04586e61bff9d3f1fd76331b35a0ea62a1c22e0
Andre Goncalves <andreg2@illinois.edu>
2023-07-19 15:41:16 -0300

    add mouseover

--- worldpopindicator.js
+++ worldpopindicator.js
@@ -1,9 +1,22 @@
 var worldpop = new Array();
 var worldpopFiltered = new Array();
 
+function handleMouseOver(d) {
+    let entry = d.toElement.__data__;
+    d3.select(this).style("fill", "orange");
+    tooltip.transition().duration(200).style("opacity", 0.9);
+    tooltip.html(`<strong>Make:</strong> ${entry.Year}<br><strong>Average City MPG:</strong> ${entry.Population}<br>`)
+        .style("left", `${d3.event.pageX}px`)
+        .style("top", `${d3.event.pageY}px`);
+}
+
+function handleMouseOut() {
+    d3.select(this).style("fill", "steelblue");
+    tooltip.transition().duration(200).style("opacity", 0);
+}
+
 function loadScene1() {
     worldpopFiltered = worldpop.filter((entry) => entry.Type == 'World');
-    // console.log(worldpopFiltered);
 
     var x = d3.scaleBand().domain([2017,2018,2019,2020,2021]).range([0,200]);
     var y = d3.scaleLinear().domain([0,8000000]).range([200,0]);
@@ -18,15 +31,17 @@ function loadScene1() {
         .enter()
         .append("rect")
         .attr("x", function(d) { 
-            return x(d.Year); 
+            return x(Number(d.Year)); 
         })
         .attr("y", function(d) {
             return y(Number(d.Population)); 
         })
         .attr("width", x.bandwidth())
         .attr("height", function(d) { 
-            return 200 - y(Number(d.Population)); 
-        });
+            return 200 - y(Number(d.Population))
+        })
+        .on("mouseover", handleMouseOver)
+        .on("mouseout", handleMouseOut);
 
     d3.select("svg").append("g")
         .attr("transform", "translate(50,50)")
@@ -40,7 +55,6 @@ function loadScene1() {
 async function init() {
     // Fetch cars data
     worldpop = await d3.csv("world_pop_filtered.csv");
-    console.log(worldpop);
 
     // Load scene 1
     loadScene1();
